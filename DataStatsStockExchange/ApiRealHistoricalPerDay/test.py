79e9be435575a3115b86b1649fd7c2b719bae97b
Ivan Lorenzo <ivanlmediavilla@gmail.com>
2020-05-10 22:21:53 +0200

    Changes in stats tratement

--- DataStatsStockExchange/ApiRealHistoricalPerDay/test.py
+++ DataStatsStockExchange/ApiRealHistoricalPerDay/test.py
@@ -11,18 +11,9 @@ END_DATE = str((datetime.today() - timedelta(days_to_subtract)).strftime('%Y-%m-
 
 # print(END_DATE)
 
-UK_STOCK = 'AUTO:LN' #FTSE 100
+UK_STOCK = 'AUTO.L' #FTSE 100
 USA_STOCK = 'AMZN' #NASDAQ
 SP_STOCK = 'ANA.MC' #IBEX 35
-
-def get_stats(stock_data):
-    return{
-        'last': np.mean(stock_data.tail(1)),
-        'shot_mean': np.mean(stock_data.tail(20)),
-        'long_mean': np.mean(stock_data.tail(200)),
-        'short_rolling': stock_data.rolling(window=20).mean(),
-        'long_rolling': stock_data.rolling(window=200).mean()
-    }
 
 def clean_data(stock_data, col):
     weekdays = pd.date_range(start = START_DATE, end = END_DATE)

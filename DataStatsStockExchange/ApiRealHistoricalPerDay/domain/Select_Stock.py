46aec1c7c3862dae4d637b925d2cad335771f380
Ivan Lorenzo <ivanlmediavilla@gmail.com>
2020-05-15 19:42:01 +0200

    first select

--- DataStatsStockExchange/ApiRealHistoricalPerDay/domain/Select_Stock.py
+++ DataStatsStockExchange/ApiRealHistoricalPerDay/domain/Select_Stock.py
@@ -1,4 +1,11 @@
 from DataStatsStockExchange.ApiRealHistoricalPerDay.domain.ConnectionDB import con
 
-con()
+def select_url_stock_exchange(con):
+    db = con
+    curs = con.cursor()
+    sql_sentence = "select url from stocks_exchange"
+    curs.execute(sql_sentence)
+    record = curs.fetchall()
+    print(record)
 
+select_url_stock_exchange(con())
